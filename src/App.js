8232290281f0767281612681a7855276da445387
Mini Kim <mini150146@gmail.com>
2019-12-10 14:26:48 +0900

    :sparkles: Create componentDidUpdate

--- src/App.js
+++ src/App.js
@@ -4,11 +4,26 @@ import './App.css';
 class App extends React.Component {
   constructor() {
     super()
+    this.state= { sprint: 'chatterbox' }
+    console.log('constructor 실행')
   }
+
+
+  changeSprint = ()=>{
+    this.setState({sprint: 'react'})
+    console.log('setState 실행')
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if(prevState.sprint !== this.state.sprint) console.log('componentDidUpdate 실행')
+  }
+
   render() {
-    return (
-      <div>react sprint office hour</div>
-    )
+    const { sprint } = this.state
+    console.log('render 실행')
+      return sprint.length ? 
+        (<><div>{this.state.sprint} sprint office hour</div>
+        <button onClick={this.changeSprint}>next</button></>) : (<div>Loading..</div>)
   }
 }
 
