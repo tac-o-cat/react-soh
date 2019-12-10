06efaad45d0c38ce970405234d5e0318d97936db
Mini Kim <mini150146@gmail.com>
2019-12-10 15:41:07 +0900

    :sparkles: Create umount demo

--- src/App.js
+++ src/App.js
@@ -2,13 +2,37 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
+    this.state = { show: true }
+  }
+
+  hideHeader = ()=>{
+    this.setState({show: false})
+  }
+
+  render() {
+    let myheader;
+    if (this.state.show) {
+      myheader = <Child />;
+    };
+    return (
+      <div>
+      {myheader}
+      <button type="button" onClick={this.hideHeader}>헤더 지우기</button>
+      </div>
+    );
+  }
+}
+
+class Child extends React.Component {
+  componentWillUnmount() {
+    alert("헤더 컴포넌트가 unmount합니다..");
   }
   render() {
     return (
-      <div>react sprint office hour</div>
-    )
+      <h1>저를 지우지 마세요😭😭</h1>
+    );
   }
 }
 
