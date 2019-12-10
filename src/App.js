2aac808788fd94d06098be9fc1623fb22f21f125
Mini Kim <mini150146@gmail.com>
2019-12-10 13:50:55 +0900

    :sparkles: Create mount phase example

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import './App.css';
 class App extends React.Component {
   constructor() {
     super()
+    console.log('constructor 실행')
+  }
+  componentDidMount(){
+    console.log('componentDidMount 실행')
   }
   render() {
+    console.log('render 실행')
     return (
       <div>react sprint office hour</div>
     )
